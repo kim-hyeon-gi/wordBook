4816737d72e5f9f07f521d4805f041f0b69e38d5
kk1624@naver.com <kk1624@naver.com>
2020-12-05 00:26:30 +0900

    add Create and Destroy function

---
+++ wordBook/wordBook/wordBook.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Create(WordBook* wordBook, Long capacity) {
+	wordBook->words = (Word(*))calloc(capacity, sizeof(Word));
+	wordBook->capacity = capacity;
+	wordBook->length = 0;
+}
+void Destroy(WordBook* wordBook) {
+	if (wordBook->words != NULL) {
+		free(wordBook->words);
+		printf("%s", "할당 해제되었습니다.");
+	}
+}
