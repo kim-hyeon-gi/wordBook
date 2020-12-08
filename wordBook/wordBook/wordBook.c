e1a4de4f95997f8b8073c160c0097629b7de25b2
kk1624@naver.com <kk1624@naver.com>
2020-12-09 00:46:45 +0900

    add Long Load

--- wordBook/wordBook/wordBook.c
+++ wordBook/wordBook/wordBook.c
@@ -1,4 +1,4 @@
-#include <wordBook.h>
+ï»¿#include <wordBook.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +11,39 @@ void Create(WordBook* wordBook, Long capacity) {
 void Destroy(WordBook* wordBook) {
 	if (wordBook->words != NULL) {
 		free(wordBook->words);
-		printf("%s", "ÇÒ´ç ÇØÁ¦µÇ¾ú½À´Ï´Ù.");
+		printf("%s", "í• ë‹¹ í•´ì œë˜ì—ˆìŠµë‹ˆë‹¤.");
 	}
+}
+
+Long Load(WordBook* wordBook) {
+	Word(*words) = NULL;
+	Word word;
+	Long index;
+	Long i = 0;
+	size_t flag;
+	FILE* file;
+	file = fopen("WordBook.dat", "rb");
+	if (file != NULL) {
+		flag = fread(&word, sizeof(Word), 1, file);
+		while (!feof(file) && flag != 0) {
+			if (wordBook->length >= wordBook->capacity) {
+				words = (Word(*))calloc(wordBook->capacity + 1, sizeof(Word));
+				while (i < wordBook->length) {
+					words[i] = wordBook->words[i];
+					i++;
+				}
+				if (wordBook->words != NULL) {
+					free(wordBook->words);
+				}
+				wordBook->words = words;
+				wordBook->capacity++;
+			}
+			index = wordBook->length;
+			wordBook->words[index] = word;
+			wordBook->length++;
+			flag = fread(&word, sizeof(Word), 1, file);
+		}
+		fclose(file);
+	}
+	return wordBook->length;
 }
