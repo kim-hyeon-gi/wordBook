c72c8930532657e8fd7dbd0c2f249d8a2c16bfd5
kimhyeongi <kimhun0505@naver.com>
2020-12-12 15:08:35 +0900

    RecordButtonClicked 오류 수정, FindForm 형시 추가, 실행 가능

--- wordBook/wordBook/wordBook/wordBookForm.h
+++ wordBook/wordBook/wordBook/wordBookForm.h
@@ -8,4 +8,6 @@ BOOL WordBookForm_OnInitDialog(HWND hWnd, WPARAM wParam, LPARAM lParam);
 BOOL WordBookForm_OnClose(HWND hWnd, WPARAM wParam, LPARAM lParam);
 BOOL WordBookForm_OnCommand(HWND hWnd, WPARAM wParam, LPARAM lParam);
 BOOL WordBookForm_OnRecordButtonClicked(HWND hWnd, WPARAM wParam, LPARAM lParam);
+BOOL WordBookForm_OnFindButtonClicked(HWND hWnd, WPARAM wParam, LPARAM lParam);
+BOOL WordBookForm_OnCorrectButtonClicked(HWND hWnd, WPARAM wParam, LPARAM lParam);
 #endif // _WORDBOOKFORM_H
